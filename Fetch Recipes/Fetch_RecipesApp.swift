895eb2238321f385249bf550a9c59924f1680bc3
rina yadav <love2002.year@gmail.com>
2025-06-01 22:38:48 -0500

    Initial Commit

---
+++ Fetch Recipes/Fetch_RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_RecipesApp.swift
+//  Fetch Recipes
+//
+//  Created by rina yadav on 6/1/25.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Fetch RecipesTests/Fetch_RecipesTests.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_RecipesTests.swift
+//  Fetch RecipesTests
+//
+//  Created by rina yadav on 6/1/25.
+//
+
+import Testing
+@testable import Fetch_Recipes
+
+struct Fetch_RecipesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
